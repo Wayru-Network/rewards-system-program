623286a1b6136c280f70b5ea26863c007bcc2c70
slipiduche <alejandrocamacaro91@gmail.com>
2024-12-11 00:14:46 -0400

    initialize and update test works

--- programs/nfnode-rewards/src/lib.rs
+++ programs/nfnode-rewards/src/lib.rs
@@ -1,12 +1,8 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, Token, Transfer};
-use solana_program::{
-    ed25519_program,
-    instruction::Instruction,
-    program::invoke,
-};
+use anchor_spl::token::{ self, Token, Transfer };
+use solana_program::{ ed25519_program, instruction::Instruction, program::invoke };
 
-declare_id!("3bJrkegLA6EmU5gmoXNHwnSiVoVbZVkqSsaa7dfV6e2e");
+declare_id!("4E4oAH7z8bFYrzhovCqq9VgMgqAW9oJ6jHfmMS2Kiork");
 
 const MANUFACTURER_SHARE: u8 = 1; // Always 1%
 
@@ -21,7 +17,7 @@ pub mod reward_system {
         host_share: u8,
         admin_pubkey: [u8; 32],
         admin_signature: [u8; 64],
-        nonce: u64,
+        nonce: u64
     ) -> Result<()> {
         let reward_entry_key = ctx.accounts.reward_entry.key();
 
@@ -37,36 +33,63 @@ pub mod reward_system {
             reward_amount,
             owner_share,
             host_share,
-            nonce,
+            nonce
         );
 
         // Verify admin signature using Ed25519 program
-        verify_ed25519_signature(
-            &admin_pubkey,
-            &admin_signature,
-            &message,
-        )?;
+        verify_ed25519_signature(&admin_pubkey, &admin_signature, &message)?;
 
         // Calculate amounts
-        let (owner_amount, host_amount, manufacturer_amount) =
-            calculate_amounts(reward_amount, owner_share, host_share);
+        let (owner_amount, host_amount, manufacturer_amount) = calculate_amounts(
+            reward_amount,
+            owner_share,
+            host_share
+        );
 
         // Transfers
-        token::transfer(
-            ctx.accounts.transfer_to_owner(),
-            owner_amount,
-        )?;
-        token::transfer(
-            ctx.accounts.transfer_to_host(),
-            host_amount,
-        )?;
-        token::transfer(
-            ctx.accounts.transfer_to_manufacturer(),
-            manufacturer_amount,
-        )?;
+        token::transfer(ctx.accounts.transfer_to_owner(), owner_amount)?;
+        token::transfer(ctx.accounts.transfer_to_host(), host_amount)?;
+        token::transfer(ctx.accounts.transfer_to_manufacturer(), manufacturer_amount)?;
 
         // Register the nonce as claimed
         ctx.accounts.reward_entry.claimed_nonces.push(nonce);
+
+        Ok(())
+    }
+
+    // Method to update the admin public key
+    pub fn update_admin(ctx: Context<UpdateAdmin>, new_admin_pubkey: [u8; 32]) -> Result<()> {
+        // First, get the admin_account key immutably
+        let admin_account = &mut ctx.accounts.admin_account;
+
+        // Then, check that the signer is the deployer of the program
+        let user_key_base58 = ctx.accounts.user.key().to_string();
+        let admin_key_base58 = Pubkey::from(admin_account.admin_pubkey).to_string();
+
+        msg!("User key: {}", user_key_base58);
+        msg!("Admin key: {}", admin_key_base58);
+        require!(user_key_base58 == admin_key_base58, RewardError::Unauthorized);
+
+        admin_account.admin_pubkey = new_admin_pubkey; // Update the admin public key
+        Ok(())
+    }
+
+    // General initialization function
+    pub fn initialize_system(ctx: Context<InitializeSystem>) -> Result<()> {
+        // Initialize the admin account
+        let admin_account = &mut ctx.accounts.admin_account;
+        admin_account.admin_pubkey = ctx.accounts.user.key().to_bytes();
+        // Initialize the reward_entry PDA
+        let (reward_entry_key, _bump) = Pubkey::find_program_address(
+            &[b"reward_entry", ctx.accounts.user.key().as_ref()],
+            ctx.program_id
+        );
+
+        // Ensure that the reward_entry account is the expected PDA
+        require!(ctx.accounts.reward_entry.key() == reward_entry_key, RewardError::InvalidAccount);
+
+        let reward_entry = &mut ctx.accounts.reward_entry;
+        reward_entry.claimed_nonces = vec![]; // Initialize the nonces vector
 
         Ok(())
     }
@@ -98,9 +121,49 @@ pub struct ClaimRewards<'info> {
     pub admin: UncheckedAccount<'info>,
 }
 
+// Context struct for the general initialization
+#[derive(Accounts)]
+pub struct InitializeSystem<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>, // User who is initializing
+    #[account(
+        init,
+        payer = user,
+        space = 40,
+        seeds = [b"admin_account", user.key().as_ref()],
+        bump
+    )] //
+    pub admin_account: Account<'info, AdminAccount>, // Admin account
+    #[account(
+        init,
+        payer = user,
+        space = 1024,
+        seeds = [b"reward_entry", user.key().as_ref()],
+        bump
+    )] // Space for reward_entry
+    pub reward_entry: Account<'info, RewardEntry>, // Reward entry PDA
+    pub system_program: Program<'info, System>, // Required for initialization
+}
+#[derive(Accounts)]
+pub struct UpdateAdmin<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>, //
+    #[account(
+        mut,
+        seeds = [b"admin_account", user.key().as_ref()],
+        bump
+    )] //
+    pub admin_account: Account<'info, AdminAccount>, // Admin account
+    pub system_program: Program<'info, System>, // Required for initialization
+}
 #[account]
 pub struct RewardEntry {
     pub claimed_nonces: Vec<u64>, // Stores nonces to prevent replay attacks
+}
+
+#[account]
+pub struct AdminAccount {
+    pub admin_pubkey: [u8; 32], // Stores the public key of the admin
 }
 
 #[error_code]
@@ -109,6 +172,10 @@ pub enum RewardError {
     RewardAlreadyClaimed,
     #[msg("Invalid admin signature.")]
     InvalidAdminSignature,
+    #[msg("Invalid rewards account.")]
+    InvalidAccount,
+    #[msg("Unauthorized access.")]
+    Unauthorized,
 }
 
 // Helper function to generate the signature message
@@ -117,7 +184,7 @@ fn generate_reward_message(
     reward_amount: u64,
     owner_share: u8,
     host_share: u8,
-    nonce: u64,
+    nonce: u64
 ) -> Vec<u8> {
     let mut message = vec![];
     message.extend_from_slice(reward_entry.as_ref());
@@ -132,7 +199,7 @@ fn generate_reward_message(
 fn verify_ed25519_signature(
     public_key: &[u8; 32],
     signature: &[u8; 64],
-    message: &[u8],
+    message: &[u8]
 ) -> Result<()> {
     let instruction_data = create_ed25519_instruction_data(signature, public_key, message);
 
@@ -146,11 +213,7 @@ fn verify_ed25519_signature(
 }
 
 // Helper function to create Ed25519 instruction data
-fn create_ed25519_instruction_data(
-    signature: &[u8],
-    public_key: &[u8],
-    message: &[u8],
-) -> Vec<u8> {
+fn create_ed25519_instruction_data(signature: &[u8], public_key: &[u8], message: &[u8]) -> Vec<u8> {
     let mut instruction_data = vec![];
 
     // Calculate offsets
@@ -159,9 +222,9 @@ fn create_ed25519_instruction_data(
 
     // Add instruction prefix (required by the Ed25519 program)
     instruction_data.push(0); // Signature verification instruction
-    instruction_data.extend_from_slice(&(64_u32.to_le_bytes())); // Signature length
-    instruction_data.extend_from_slice(&(0_u32.to_le_bytes())); // Signature offset
-    instruction_data.extend_from_slice(&(32_u32.to_le_bytes())); // Public key length
+    instruction_data.extend_from_slice(&(64_u32).to_le_bytes()); // Signature length
+    instruction_data.extend_from_slice(&(0_u32).to_le_bytes()); // Signature offset
+    instruction_data.extend_from_slice(&(32_u32).to_le_bytes()); // Public key length
     instruction_data.extend_from_slice(&(pubkey_offset as u32).to_le_bytes()); // Public key offset
     instruction_data.extend_from_slice(&(message.len() as u32).to_le_bytes()); // Message length
     instruction_data.extend_from_slice(&(message_offset as u32).to_le_bytes()); // Message offset
@@ -175,43 +238,34 @@ fn create_ed25519_instruction_data(
 }
 
 fn calculate_amounts(reward_amount: u64, owner_share: u8, host_share: u8) -> (u64, u64, u64) {
-    let owner_amount = reward_amount * owner_share as u64 / 100;
-    let host_amount = reward_amount * host_share as u64 / 100;
-    let manufacturer_amount = reward_amount * MANUFACTURER_SHARE as u64 / 100;
+    let owner_amount = (reward_amount * (owner_share as u64)) / 100;
+    let host_amount = (reward_amount * (host_share as u64)) / 100;
+    let manufacturer_amount = (reward_amount * (MANUFACTURER_SHARE as u64)) / 100;
     (owner_amount, host_amount, manufacturer_amount)
 }
 
 impl<'info> ClaimRewards<'info> {
     fn transfer_to_owner(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
-        CpiContext::new(
-            self.token_program.to_account_info(),
-            Transfer {
-                from: self.owner_token_account.to_account_info(),
-                to: self.owner.to_account_info(),
-                authority: self.owner.to_account_info(),
-            },
-        )
+        CpiContext::new(self.token_program.to_account_info(), Transfer {
+            from: self.owner_token_account.to_account_info(),
+            to: self.owner.to_account_info(),
+            authority: self.owner.to_account_info(),
+        })
     }
 
     fn transfer_to_host(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
-        CpiContext::new(
-            self.token_program.to_account_info(),
-            Transfer {
-                from: self.host_token_account.to_account_info(),
-                to: self.host.to_account_info(),
-                authority: self.owner.to_account_info(),
-            },
-        )
+        CpiContext::new(self.token_program.to_account_info(), Transfer {
+            from: self.host_token_account.to_account_info(),
+            to: self.host.to_account_info(),
+            authority: self.owner.to_account_info(),
+        })
     }
 
     fn transfer_to_manufacturer(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
-        CpiContext::new(
-            self.token_program.to_account_info(),
-            Transfer {
-                from: self.manufacturer_token_account.to_account_info(),
-                to: self.manufacturer.to_account_info(),
-                authority: self.owner.to_account_info(),
-            },
-        )
+        CpiContext::new(self.token_program.to_account_info(), Transfer {
+            from: self.manufacturer_token_account.to_account_info(),
+            to: self.manufacturer.to_account_info(),
+            authority: self.owner.to_account_info(),
+        })
     }
 }
--- tests/nfnode-rewards.ts
+++ tests/nfnode-rewards.ts
@@ -1,59 +1,148 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { RewardSystem } from "../target/types/reward_system";
+import { PublicKey, Keypair, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import nacl from "tweetnacl";
+import { strict as assert } from "assert";
+async function airdropSolIfNeeded(
+  signer: Keypair,
+  connection: Connection
+) {
+  const balance = await connection.getBalance(signer.publicKey)
+  console.log("Current balance is", balance / LAMPORTS_PER_SOL)
 
-describe("nfnode-rewards", () => {
+  // if (balance < web3.LAMPORTS_PER_SOL) {
+  if (balance < 10000) {
+
+    console.log("Airdropping 1 SOL...")
+    const airdropSignature = await connection.requestAirdrop(
+      signer.publicKey,
+      LAMPORTS_PER_SOL
+    )
+
+    const latestBlockHash = await connection.getLatestBlockhash()
+
+    await connection.confirmTransaction({
+      blockhash: latestBlockHash.blockhash,
+      lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+      signature: airdropSignature,
+    })
+
+    const newBalance = await connection.getBalance(signer.publicKey)
+    console.log("New balance is", newBalance / LAMPORTS_PER_SOL)
+  }
+}
+
+
+// Función para generar la PDA
+async function generatePDA(seed: string, userPublicKey: PublicKey, programId: PublicKey) {
+  // Define el seed que se usará para generar la PDA
+
+  // Genera la PDA
+  const [rewardEntryPDA, bump] = await PublicKey.findProgramAddress(
+    [Buffer.from(seed), userPublicKey.toBuffer()],
+    programId // Reemplaza con tu program ID
+  );
+
+  return { rewardEntryPDA, bump };
+}
+
+describe("nfnode-rewards", async () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.NfnodeRewards as Program<RewardSystem>;
+  const program = anchor.workspace.RewardSystem as Program<RewardSystem>;
+
+  const userKeypair = Keypair.generate();
+  const adminAccount = await generatePDA('admin_account', userKeypair.publicKey, program.programId)
+  const newUserAdminKeypair = Keypair.generate();
+  const reward_endtry = await generatePDA('reward_entry', userKeypair.publicKey, program.programId)
+  console.log('adminAccount:', adminAccount)
+  console.log('user:', newUserAdminKeypair)
+  console.log('newuseradminKeypair:', newUserAdminKeypair)
+  console.log('reward_endtry:', reward_endtry)
+  // Initialize the admin account
+  it("should update admin public key", async () => {
+    await airdropSolIfNeeded(userKeypair, anchor.getProvider().connection)
+    await airdropSolIfNeeded(newUserAdminKeypair, anchor.getProvider().connection)
+    // Setup: Initialize the admin account
+    const txInit = await program.methods.initializeSystem().accounts({
+      user: userKeypair.publicKey,
+    }).signers([userKeypair]).rpc({ commitment: 'confirmed' });
+    console.log('txInit:', txInit)
+    assert.ok(txInit)
+
+    // Fetch the user admin account
+    const userAdminAccount = await program.account.adminAccount.fetch(adminAccount.rewardEntryPDA, 'confirmed');
+    console.log('user admin account', userAdminAccount)
+
+    // Update the admin public key
+    await program.methods.updateAdmin(Array.from(newUserAdminKeypair.publicKey.toBytes())).accounts({
+      user: userKeypair.publicKey,
+    }).signers([userKeypair]).rpc({ commitment: 'confirmed' });
+
+    // Fetch the updated admin account
+
+    const updatedAdminAccount = await program.account.adminAccount.fetch(adminAccount.rewardEntryPDA);
+    console.log('updatedAdminAccount:', updatedAdminAccount)
+    // Assert that the admin public key has been updated
+    assert.equal(updatedAdminAccount.adminPubkey.toString(), newUserAdminKeypair.publicKey.toBytes().toString());
+  });
 
   it("claimed!", async () => {
     // Backend signature.
     // Example inputs
-  const rewardEntryKey = new PublicKey("Fg6PaFpoGXkYsidMpWxTWyVvYX2HVvTT9dVsi4d44wge");
-  const rewardAmount = 1000; // Example reward amount
-  const ownerShare = 60; // Example owner share: 60%
-  const hostShare = 39; // Example host share: 39%
-  const nonce = 12345; // Example nonce
+    const rewardEntryKey = reward_endtry.rewardEntryPDA;
+    const rewardAmount = 1000; // Example reward amount
+    const ownerShare = 60; // Example owner share: 60%
+    const hostShare = 39; // Example host share: 39%
+    const nonce = 12345; // Example nonce
 
-  // Admin Keypair (use a secure method to load this keypair)
-  const adminKeypair = Keypair.generate(); // Replace with admin keypair from .env
+    // Admin Keypair (use a secure method to load this keypair)
+    //const adminKeypair = Keypair.generate(); // Replace with admin keypair from .env
 
-  // Generate the message
-  const message = generateMessage(
-    rewardEntryKey,
-    rewardAmount,
-    ownerShare,
-    hostShare,
-    nonce
-  );
+    // Generate the message
+    const message = generateMessage(
+      rewardEntryKey,
+      rewardAmount,
+      ownerShare,
+      hostShare,
+      nonce
+    );
 
-  console.log("Generated Message (Hex):", Buffer.from(message).toString("hex"));
+    console.log("Generated Message (Hex):", Buffer.from(message).toString("hex"));
 
-  // Sign the message
-  const signature = signMessage(message, adminKeypair);
+    // Sign the message
+    const signature = signMessage(message, newUserAdminKeypair);
 
-  console.log("Admin Public Key:", adminKeypair.publicKey.toBase58());
-  console.log("Message Signature (Hex):", Buffer.from(signature).toString("hex"));
+    console.log("Admin Public Key:", newUserAdminKeypair.publicKey.toBase58());
+    console.log("Message Signature (Hex):", Buffer.from(signature).toString("hex"));
 
-
-  /////front interaction
+    // Front interaction
     const tx = await program.methods.claimRewards(
       new anchor.BN(rewardAmount),
       ownerShare,
       hostShare,
-      Array.from(adminKeypair.publicKey.toBytes()),
+      Array.from(newUserAdminKeypair.publicKey.toBytes()),
       Array.from(signature),
       new anchor.BN(nonce)
-    );
+    ).accounts({
+      rewardEntry: rewardEntryKey,
+      admin: userKeypair.publicKey,
+      host: userKeypair.publicKey,
+      owner: userKeypair.publicKey,
+      hostTokenAccount: userKeypair.publicKey,
+      ownerTokenAccount: userKeypair.publicKey,
+      manufacturer: userKeypair.publicKey,
+      manufacturerTokenAccount: userKeypair.publicKey,
+    }).signers([
+      userKeypair,
+    ]).rpc({ commitment: 'confirmed' });
+
     console.log("Your transaction signature", tx);
   });
 });
 
-
-import { PublicKey, Keypair } from "@solana/web3.js";
-import nacl from "tweetnacl";
 /**
  * Generates a message for signing.
  * @param rewardEntryKey - PublicKey of the reward entry account.
