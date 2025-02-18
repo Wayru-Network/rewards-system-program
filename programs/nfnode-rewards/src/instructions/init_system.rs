5693ae2b4857ee1041c26b6898f8b03479378779
slipiduche <alejandrocamacaro91@gmail.com>
2025-02-18 11:08:59 -0400

    solving "finding hal-10"

--- programs/nfnode-rewards/src/instructions/init_system.rs
+++ programs/nfnode-rewards/src/instructions/init_system.rs
@@ -1,15 +1,20 @@
+use crate::{
+    state::{AdminAccount, MAX_MINT_AUTHORITIES},
+    NfnodeRewards,
+};
 use anchor_lang::prelude::*;
 use anchor_spl::{
-    token::{ Token, Mint }, //Wayru Token
+    token::{Mint, Token}, //Wayru Token
 };
-use crate::{ state::{AdminAccount,MAX_MINT_AUTHORITIES}, NfnodeRewards };
 
 pub fn initialize_system(ctx: Context<InitializeSystem>) -> Result<()> {
     let admin_account = &mut ctx.accounts.admin_account;
     admin_account.admin_pubkey = ctx.accounts.user.key();
     admin_account.paused = false;
     admin_account.valid_mint = ctx.accounts.token_mint.key();
-    admin_account.mint_authorities.push(ctx.accounts.mint_authority.key());
+    admin_account
+        .mint_authorities
+        .push(ctx.accounts.mint_authority.key());
     Ok(())
 }
 
@@ -28,6 +33,7 @@ pub struct InitializeSystem<'info> {
     )]
     pub admin_account: Account<'info, AdminAccount>,
     ///CHECK: only read account
+    #[account(constraint = program_data.upgrade_authority_address == Some(mint_authority.key()))]
     pub mint_authority: AccountInfo<'info>,
     ///CHECK: only read account
     pub token_mint: Account<'info, Mint>,
